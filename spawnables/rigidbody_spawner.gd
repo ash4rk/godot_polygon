5d423e475d80b35b8d74a2e397ad4f0ab6eb138b
ash4rk <cool.shark.98@mail.ru>
2023-03-24 18:12:12 +0300

    FEAT: Add rigidbody scene

--- spawnables/rigidbody_spawner.gd
+++ spawnables/rigidbody_spawner.gd
@@ -1,8 +1,9 @@
 extends Node
 
-const SIMPLE_SPRITE = preload("res://simple_sprite/simple_sprite.tscn")
+const SIMPLE_RIGIDBODY = preload("res://spawnables/simple_rigid_body.tscn")
 @onready var Log : RichTextLabel = $CanvasLayer/LogBG/Log
 var sprite_numbers: int = 0
+var object_size: = Vector2(1.0, 1.0)
 
 func _print_log(text_value : String) -> void:
 	Log.text += text_value + "\n"
@@ -12,10 +13,14 @@ func _ready():
 	_print_log("Window size: " + str(DisplayServer.window_get_size()))
 
 func _on_spawn_sprite_button_button_up():
-	var simple_sprite_instance = SIMPLE_SPRITE.instantiate()
-	$Level.add_child(simple_sprite_instance)
+	var simple_rigidbody_instance = SIMPLE_RIGIDBODY.instantiate()
+	$Level.add_child(simple_rigidbody_instance)
 	var x_screen = DisplayServer.window_get_size().x
 	var y_screen = DisplayServer.window_get_size().y
-	simple_sprite_instance.global_position = Vector2(randf_range(0, x_screen),randf_range(0, y_screen))
+	simple_rigidbody_instance.global_position = Vector2(randf_range(0, x_screen),randf_range(0, y_screen))
 	sprite_numbers += 1
 	_print_log("Added simple sprite " + str(sprite_numbers))
+
+
+func _on_h_slider_value_changed(value):
+	$CanvasLayer/HSlider/SizeLabel.text = "Size: " + str(value)
