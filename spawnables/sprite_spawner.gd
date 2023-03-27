2e40c5a1d7f6a643ba87bfb025b198daefe41adb
ash4rk <cool.shark.98@mail.ru>
2023-03-27 16:35:07 +0300

    FEAT: Add spawn slider for simple sprites

--- spawnables/sprite_spawner.gd
+++ spawnables/sprite_spawner.gd
@@ -2,7 +2,7 @@ extends Node
 
 const SIMPLE_SPRITE = preload("res://spawnables/simple_sprite.tscn")
 @onready var Log : RichTextLabel = $CanvasLayer/LogBG/Log
-var sprite_numbers: int = 0
+var sprite_numbers: int = 500
 
 func _print_log(text_value : String) -> void:
 	Log.text += text_value + "\n"
@@ -12,10 +12,16 @@ func _ready():
 	_print_log("Window size: " + str(DisplayServer.window_get_size()))
 
 func _on_spawn_sprite_button_button_up():
-	var simple_sprite_instance = SIMPLE_SPRITE.instantiate()
-	$Level.add_child(simple_sprite_instance)
-	var x_screen = DisplayServer.window_get_size().x
-	var y_screen = DisplayServer.window_get_size().y
-	simple_sprite_instance.global_position = Vector2(randf_range(0, x_screen),randf_range(0, y_screen))
-	sprite_numbers += 1
-	_print_log("Added simple sprite " + str(sprite_numbers))
+	
+	for i in sprite_numbers:
+		var simple_sprite_instance = SIMPLE_SPRITE.instantiate()
+		$Level.add_child(simple_sprite_instance)
+		var x_screen = DisplayServer.window_get_size().x
+		var y_screen = DisplayServer.window_get_size().y
+		simple_sprite_instance.global_position = Vector2(randf_range(0, x_screen),randf_range(0, y_screen))
+
+	_print_log("Added " + str(sprite_numbers) + " simple sprites")
+
+func _on_h_slider_value_changed(value):
+	sprite_numbers = value
+	$CanvasLayer/HSlider/SizeLabel.text = "Number of objects: " + str(value)
