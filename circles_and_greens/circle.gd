5e1457d8745c1dfb22d2221c8c868fc4d9cc8184
ash4rk <cool.shark.98@mail.ru>
2023-04-25 19:03:37 +0300

    FEAT: Implement grow and damage

---
+++ circles_and_greens/circle.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+class_name Circle
+
+const INIT_SPEED = 400
+const GROW_SPEED = 0.5
+@export var speed = 400
+@export var level: float = 1.0 : set = _set_level
+
+var _is_dead: bool = false
+var _is_grows: bool = false
+var _is_damaged: bool = false
+
+func _process(delta):
+	$LevelLabel.text = str(level).pad_decimals(2)
+	if _is_grows:
+		level += delta * GROW_SPEED
+	if _is_damaged:
+		level -= delta * GROW_SPEED
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("players") and body != self:
+		_is_damaged = true
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("players"):
+		_is_damaged = false
+
+func _set_level(value):
+	if value < 1.0:
+		_is_dead = true
+		queue_free()
+		return
+	level = value
+	scale = Vector2(level, level)
+	speed = INIT_SPEED /level
--- circles_and_greens/player.gd
+++ circles_and_greens/player.gd
@@ -1,14 +1,8 @@
-extends CharacterBody2D
+extends Circle
 
-@export var speed = 400
-var level: float = 1.0
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	var target = get_global_mouse_position()
 	velocity = position.direction_to(target) * speed
-	look_at(target)
+	#look_at(target)
 	if position.distance_to(target) > 10:
 		move_and_slide()
-
-func _process(delta):
-	$LevelLabel.text = str(level)
