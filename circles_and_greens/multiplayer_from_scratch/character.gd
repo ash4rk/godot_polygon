09031601c2b0c2c66397057d9a58ccb5fb867d8e
ash4rk <cool.shark.98@mail.ru>
2023-04-30 19:11:19 +0300

    FIX: Implement grow and take damage

--- circles_and_greens/multiplayer_from_scratch/character.gd
+++ circles_and_greens/multiplayer_from_scratch/character.gd
@@ -2,12 +2,18 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+const INIT_SPEED = 400
+const GROW_SPEED = 0.5
+
 func _ready():
 	position = $Networking.sync_position
 	if str(name).is_valid_int():
 		$Networking/MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
 
 func _physics_process(_delta):
+	if $Networking.sync_is_dead:
+		return
+	
 	if !is_local_authority():
 		if not $Networking.processed_position:
 			position = $Networking.sync_position
@@ -26,5 +32,30 @@ func _physics_process(_delta):
 	$Networking.sync_position = position
 	$Networking.sync_velocity = velocity
 
+func _process(_delta):
+	$LevelLabel.text = str($Networking.sync_level).pad_decimals(2)
+
 func is_local_authority():
 	return $Networking/MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id()
+
+
+func _on_area_2d_area_entered(area):
+	var body = area.get_parent()
+	if body.is_in_group("growers") and !body.is_active:
+		$Networking.sync_is_grows = true
+		body.is_active = true
+
+func _on_area_2d_area_exited(area):
+	var body = area.get_parent()
+	if body.is_in_group("growers"):
+		$Networking.sync_is_grows = false
+		body.is_active = false
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("characters") and body != self:
+		$Networking.sync_is_damaged = true
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("characters"):
+		$Networking.sync_is_damaged = false
+
--- circles_and_greens/multiplayer_from_scratch/player_networking.gd
+++ circles_and_greens/multiplayer_from_scratch/player_networking.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@onready var character = get_parent()
+
 var sync_position : Vector2:
 	set(value):
 		sync_position = value
@@ -7,3 +9,25 @@ var sync_position : Vector2:
 var sync_velocity : Vector2
 
 var processed_position : bool
+
+
+var sync_level: float = 1.0 : set = _set_sync_level
+var sync_is_dead: bool = false
+var sync_is_grows: bool = false
+var sync_is_damaged: bool = false
+
+func _set_sync_level(value):
+	if value < 0.5:
+		sync_is_dead = true
+		print("Player Dead!")
+		get_parent().queue_free()
+		return
+	sync_level = value
+	character.scale = Vector2(sync_level, sync_level)
+	character.speed = character.INIT_SPEED / sync_level
+
+func _process(delta):
+	if sync_is_grows:
+		sync_level += delta * character.GROW_SPEED
+	if sync_is_damaged:
+		sync_level -= delta * character.GROW_SPEED
