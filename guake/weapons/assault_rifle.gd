9720629188f04af6e3b2c6f7eac1fbd954100fe9
ash4rk <cool.shark.98@mail.ru>
2023-05-23 11:16:22 +0300

    FEAT: Add bullet hole decals

--- guake/weapons/assault_rifle.gd
+++ guake/weapons/assault_rifle.gd
@@ -1,5 +1,7 @@
 extends Weapon
 
+@onready var DECAL_SCENE = preload("res://weapons/bullet_hole_decal.tscn")
+
 func _process(delta):
 	if not is_multiplayer_authority(): return
 		
@@ -22,20 +24,19 @@ func fire(_delta):
 
 	await get_tree().create_timer(firerate).timeout
 	if ray_cast.is_colliding():
-		_place_decal()
+		_place_decal.rpc(ray_cast.get_collision_point())
 
 	if not is_reloading:
 		can_fire = true
 
-func _place_decal():
-	pass
-#	var col_point = ray_cast.get_collision_point()
-#	var D = DECAL.instantiate()
-#	get_tree().get_root().add_child(D)
-#	D.global_transform.origin = col_point
-#	var t = get_tree().create_tween()
-#	t.tween_interval(1)
-#	t.tween_callback(func():D.free())
+@rpc("call_local")
+func _place_decal(point: Vector3):
+	var decal = DECAL_SCENE.instantiate()
+	get_tree().get_root().add_child(decal)
+	decal.global_transform.origin = point
+	var tween = get_tree().create_tween()
+	tween.tween_interval(3)
+	tween.tween_callback(func():decal.free())
 
 @rpc("call_local")
 func play_shoot_effects():
